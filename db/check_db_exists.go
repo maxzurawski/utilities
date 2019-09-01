0d44c627806b74c5eb4a3bccd1d1e58e2923ec47
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-09-01 16:22:55 +0200

    Adding checking if database exists

---
+++ db/check_db_exists.go
@@ -0,0 +1,14 @@
+package db
+
+import "os"
+
+func AdjustDBPath(dbPath string) (string, bool) {
+	var exists = true
+	if _, err := os.Stat(dbPath); err != nil {
+		exists = false
+	}
+	if !exists {
+		dbPath = dbPath + "?mode=rwc"
+	}
+	return dbPath, exists
+}
