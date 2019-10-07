081ce7012c8cb926ea9a65760e91e41bc317e601
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-07 07:10:34 +0200

    General Publishing 2

---
+++ rabbit/crosscutting/publish.go
@@ -0,0 +1,28 @@
+package crosscutting
+
+import (
+	"fmt"
+
+	"github.com/labstack/gommon/log"
+	"github.com/streadway/amqp"
+)
+
+func (r *RabbitConnector) Publish(topic, routingkey, body string) {
+	err := r.Channel.Publish(
+		topic,
+		routingkey,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(body),
+		})
+
+	if err != nil {
+		log.Errorf(
+			fmt.Sprintf("could not publish msg for topic: [%s] and routingkey: [%s]", topic, routingkey),
+			err)
+	}
+
+	log.Infof(fmt.Sprintf("msg published to topic: [%s] and routingkey: [%s]", topic, routingkey))
+}
