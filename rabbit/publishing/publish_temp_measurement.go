65f7d030a36886009ee207d36f6907fa66523d4b
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-15 14:16:46 +0200

    Initial version of Publishing temperature measurement function

---
+++ rabbit/publishing/publish_temp_measurement.go
@@ -0,0 +1,41 @@
+package publishing
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/labstack/gommon/log"
+	"github.com/xdevices/utilities/rabbit/crosscutting"
+	"github.com/xdevices/utilities/rabbit/domain"
+)
+
+func (d *Publisher) PublishTemperatureMeasurement(
+	routingKey,
+	service,
+	sensorUuid string,
+	value float64) {
+
+	now := time.Now()
+
+	msg := domain.TemperatureMeasurement{
+		ProcessId:   uuid.New().String(),
+		Value:       value,
+		Service:     service,
+		PublishedOn: now,
+		SensorUuid:  sensorUuid,
+	}
+
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		log.Warn("could not marshal msg")
+		return
+	}
+	d.Reset()
+	d.Publish(
+		crosscutting.TopicMeasurements.String(),
+		routingKey,
+		string(bytes))
+
+	d.PublishExtInfo(msg)
+}
